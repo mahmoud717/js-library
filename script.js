7a93a33210877a750798f22927f491bc07478328
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-11-24 11:19:47 +0200

    created show book function

--- script.js
+++ script.js
@@ -41,3 +41,54 @@ function addBookToLibrary(title, author, pages, read) {
     library.push(newBook)
     showBook(title, author, pages, read)
 }
+function showBook(title, author, pages, read){
+    const li = document.createElement("li")
+    const bookName = document.createElement("span")
+    const titleLabel = document.createElement("span")
+    const bookAuthor = document.createElement("span")
+    const authorLabel = document.createElement("span")
+    const bookPages = document.createElement("span")
+    const pagesLabel = document.createElement("span")
+    const bookRead = document.createElement("span")
+    const readLabel = document.createElement("span")
+    const deleteBtn = document.createElement("span")
+
+    // changing the style and content of the elements
+    bookName.textContent =  title
+    bookName.classList.add("name")
+    titleLabel.classList.add("label", "title-label")
+    titleLabel.textContent = "Title:"
+    bookAuthor.textContent = author
+    bookAuthor.classList.add("author")
+    authorLabel.classList.add("label", "author-label")
+    authorLabel.textContent = "Author:"
+    bookPages.textContent =  pages
+    bookPages.classList.add("pages")
+    pagesLabel.classList.add("label", "pages-label")
+    pagesLabel.textContent = "Number of Pages:"
+    bookRead.textContent = read
+    bookRead.classList.add("read")
+    readLabel.classList.add("label", "read-label")
+    readLabel.textContent = "Read?:"
+    deleteBtn.textContent = "delete"
+    deleteBtn.classList.add("delete")
+
+    li.appendChild(titleLabel)
+    li.appendChild(bookName)
+    li.appendChild(authorLabel)
+    li.appendChild(bookAuthor)
+    li.appendChild(pagesLabel)
+    li.appendChild(bookPages)
+    li.appendChild(readLabel)
+    li.appendChild(bookRead)
+    li.appendChild(deleteBtn)
+    $bookList.appendChild(li)
+}
+
+// render all books from library
+function renderLibrary(){
+    $bookList.innerHTML = ""
+    library.forEach((el) => {
+        showBook(el.title, el.author, el.pages, el.read)
+  });
+}
